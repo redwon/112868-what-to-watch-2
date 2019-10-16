e588e4a84423cf2ffde914ee4641aa9b91a2b196
Igor Levchenko <igorvk7@gmail.com>
2019-10-17 01:00:43 +0300

    Add main page

---
+++ src/components/app/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Main from '../main/main.jsx';
+
+const App = (props) => {
+  const {currentYear} = props;
+
+  return <Main copyrightYear={currentYear}/>;
+};
+
+App.propTypes = {
+  currentYear: PropTypes.number
+};
+
+export default App;
